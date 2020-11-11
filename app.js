da73c8d0e8d14a6142da4eda492928dfcfad34fe
adoseofsam <jamessamantha099@gmail.com>
2020-11-10 21:15:48 -0500

    Completed exercise 2

---
+++ app.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  var searchButton = document.getElementById("btn");
+  var httpRequest;
+
+  searchButton.onclick = Request;
+  function Request() {
+    event.preventDefault();
+    httpRequest = new XMLHttpRequest();
+    var userInput = document.getElementById("inputField").value;
+    var url = "superheroes.php" + "?query=" + userInput;
+
+    httpRequest.onreadystatechange = getResults;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  }
